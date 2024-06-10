6f4823e1ab68bf3b43c573664aca6130cb94dfb8
shirisha reddy <122144371+shirishareddy28@users.noreply.github.com>
2024-06-10 14:15:12 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+// next.config.js
+module.exports = {
+    env: {
+      RESEND_API_KEY: process.env.re_4qxK8b8R_7wXy8ziYhfMMEDfb5kDnCELg
+      ,
+    },
+  };
+  
